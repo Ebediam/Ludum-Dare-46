7de2f16c34db67ed4240bffb747ac2015bafe597
Ebediam <felipebiosca@gmail.com>
2020-04-17 23:00:43 +0200

    Test

---
+++ Assets/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    public Material newColor;
+
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MeshRenderer myMesh = gameObject.GetComponent<MeshRenderer>();
+
+        myMesh.material = newColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
